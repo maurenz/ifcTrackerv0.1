cb2f1d528b08c47b56dfb33d17e1d7075fefabe5
maurenz <161854135+maurenz@users.noreply.github.com>
2024-03-04 17:57:54 +0100

    Added Date Filter

--- 4visualize.py
+++ 4visualize.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+from datetime import datetime
 
 # Streamlit UI
 st.title("Ifc Tracker")
@@ -15,19 +16,24 @@ if uploaded_file is not None:
 
     # Datumsformat konvertieren
     df['Datum'] = pd.to_datetime(df['Datum'], format='%d.%m.%Y')
-    
-
 
     # Multiple choice Selektoren
+    default_start_date = datetime(2023, 12, 1)
+    default_end_date = datetime.now()
     default_cluster = ["HAA"] if "HAA" in df['Cluster'].unique() else None
     default_values_tps = [value for value in [1, 2, 3] if value in df['TP'].unique()]
 
+    start_date = st.date_input('Startdatum', value=default_start_date)
+    end_date = st.date_input('Enddatum', value=default_end_date)
     selected_clusters = st.multiselect('Wähle Cluster:', options=df['Cluster'].unique(), default=default_cluster)
     selected_tps = st.multiselect('Wähle TP:', options=df['TP'].unique(), default=[1, 2, 3])
-    
+
     # Daten basierend auf Auswahl filtern
-    filtered_data = df[df['Cluster'].isin(selected_clusters) & df['TP'].isin(selected_tps)]
-    
+    filtered_data = df[df['Cluster'].isin(selected_clusters) &
+                     df['TP'].isin(selected_tps) &
+                     (df['Datum'] >= pd.Timestamp(start_date)) &
+                     (df['Datum'] <= pd.Timestamp(end_date))]
+
     if not filtered_data.empty:
         # Visualisierung
         fig, ax = plt.subplots()
@@ -35,7 +41,7 @@ if uploaded_file is not None:
             ax.plot(group['Datum'], group['percentage'], label=f' {cluster}, TP {tp}')
             # Zeichne Kreise an jedem Datenpunkt
             ax.scatter(group['Datum'], group['percentage'], s=10)
-    
+
         # Setze die x-Ticks auf die eindeutigen Datumsangaben in den gefilterten Daten
         ax.set_xticks(filtered_data['Datum'].unique())
         ax.xaxis.set_major_formatter(mdates.DateFormatter('%d.%m.%y'))
@@ -43,38 +49,41 @@ if uploaded_file is not None:
         ax.set_ylabel('Developement [%]')
         ax.legend()
         plt.xticks(rotation=45)  # Dreht die x-Achsen-Beschriftungen für bessere Lesbarkeit
-    
+
         # Zeichne graue horizontale Linien bei jedem Y-Achsen-Label
         y_ticks = ax.get_yticks()
         for y in y_ticks:
             ax.axhline(y, color='#cccccc', linestyle='-', linewidth=0.3)
-    
+
         st.pyplot(fig)
     else:
         st.write("Bitte wähle mindestens einen Cluster und einen TP aus, um die Daten anzuzeigen.")
-    
-    
-    #df.sort_values('Datum', inplace=True)
-    
+
+
+    # df.sort_values('Datum', inplace=True)
+
     # Berechnen der Differenz "Change"
     def calculate_percentage_change(group):
         if len(group) > 1:
             return group['percentage'].iloc[-1] - group['percentage'].iloc[0]
         else:
             return 0  # Keine Veränderung, wenn nur ein Datum vorhanden ist
-    
+
+
     # Gruppieren nach "Cluster" und "TP", und Anwenden der Berechnung
-    change_df = df.groupby(['Cluster', 'TP']).apply(calculate_percentage_change).reset_index(name='Change 1. - last [%]')
-    
+    change_df = df.groupby(['Cluster', 'TP']).apply(calculate_percentage_change).reset_index(
+        name='Change 1. - last [%]')
+
     # Extrahiere den Wert von "IfcElemente" des aktuellsten Datums jeder Gruppe
-    ifc_elements_latest = df.groupby(['Cluster', 'TP']).apply(lambda x: x.loc[x['Datum'].idxmax(), 'IfcElemente']).reset_index(name='currentIfcElements')
-    
+    ifc_elements_latest = df.groupby(['Cluster', 'TP']).apply(
+        lambda x: x.loc[x['Datum'].idxmax(), 'IfcElemente']).reset_index(name='currentIfcElements')
+
     # Füge die neue Spalte "IfcElements" zu "change_df" hinzu
     change_df = pd.merge(change_df, ifc_elements_latest, on=['Cluster', 'TP'])
-    
+
     # Prozentzeichen zu den Werten in "Change 1. - last [%]" hinzufügen
     change_df['Change 1. - last [%]'] = change_df['Change 1. - last [%]']
-    
+
     # Tabelle nach "Change 1. - last [%]" aufsteigend sortieren
     change_df.sort_values('Change 1. - last [%]', inplace=True)
     change_df.reset_index(drop=True, inplace=True)
